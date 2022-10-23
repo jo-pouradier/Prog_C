f8a21732f8d0b6b933d624f4f101ed9fc49e58ad
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2022-10-23 16:07:20 +0200

    error esgmentation tour 1 note

--- TP5/src/client.c
+++ TP5/src/client.c
@@ -33,70 +33,98 @@ int envoie_message(int socketfd, char *data){
 }
 
 int envoie_numeros_operateur(int socketfd, char *data){
+  //printf("in fct\n");
   char dir[1024];
+  char delim[] =" ";
+  char *dip;
+  dip = strtok(data, delim);
+  dip = strtok(NULL, delim);
   memset(dir, 0, sizeof(dir));
-  strcat(dir, (data+10));
+  strcpy(dir, dip);
+  //on enleve le "\n" de fin
+  if( dir[strlen(dir)-1] == '\n' )
+    dir[strlen(dir)-1] = 0;
+  //printf("%s\n", dir);
 
   //on va ouvrir le dossier
   DIR *dirp = opendir(dir);
+  //printf("opening folder\n");
   struct dirent *entry;
   if (dirp == NULL) {
       printf("error opendir");
+      return(EXIT_FAILURE);
   }
+  printf("ouverture dossier ok\n");
+
   // on cree un struct eleve pour stocké les données
   typedef struct{
     char dire[1024];
     char prenom[30]; //nom de son repertoire
-    struct notes{int nbr_notes, notes[50];} notes;// pour faire sa moyenne
+    struct notes{int nbr_notes, notes[50];} notes;// pour faire sa moyenne, 50 notes max
   }eleve;
   const int NBR_ELEVE = 248;
   eleve promo[NBR_ELEVE];
 
- int n = 0; //compte le nombre d'eleve
+  //initialisation du prenom et du dossier eleve
+  int n = 0; //compte le nombre d'eleve
+  entry = readdir(dirp); // on enleve .
+  entry = readdir(dirp); // on enleve ..
+  entry = readdir(dirp); // on enleve .DS_store
   while ((entry = readdir(dirp)) != NULL){
-        strcpy((promo+n)->prenom, entry->d_name);
-        char dire[1024];
-        strcpy((promo+n)->dire, dir);
-        strcat((promo+n)->dire, entry->d_name);
-        n++;
+    strcpy((promo+n)->prenom, entry->d_name);
+    char dire[1024];
+    strcpy((promo+n)->dire, dir);
+    strcat((promo+n)->dire, entry->d_name);
+    strcat((promo+n)->dire, "/");
+    printf("prenom : %s\n", (promo+n)->prenom);
+    printf("direction : %s\n", (promo+n)->dire);
+    n++;
   }
   closedir(dirp);
   //on a mtn tout les eleves
-
+  printf("\n");
   //on recupere les notes et on fait la moyenne pour chaque eleve
-  for (int i = 0; i <NBR_ELEVE; i++){
+  for (int i = 0; i < NBR_ELEVE; i++){
+    printf("boucle eleve for tour : %i\n", i);
     //on ouvre le dossier de l'eleve
     DIR *dirp = opendir((promo+i)->dire);
     struct dirent *file;
     if (dirp == NULL) {
         printf("error opendir eleve");
     }
+    printf("eleve directory opened\n");
 
     //on considere qu'on a mtn que des fichiers avec des notes et non des dossiers
     //on lit toutes les notes de l'eleve
     (promo+i)->notes.nbr_notes = 0;
     for (int j=0; j<n; j++){
+      printf("boucle note for tour : %i\n", j);
       file = readdir(dirp);
+      
       if (file == NULL){
         printf("error readdir eleve : %s", (promo+j)->dire);
         continue;
       };
-      if (strcmp(file->d_name, ".")) entry = readdir(dirp);
-      if (strcmp(file->d_name, "..")) entry = readdir(dirp);
+      printf("fichier note readdir");
+      file = readdir(dirp); // on enleve .
+      file = readdir(dirp); // on enleve ..
+      file = readdir(dirp); // on enleve .DS_store
+      printf("%s\n", file->d_name);
 
-      
       FILE* ptr_file;
       char note[3];
       char filename[1024];
       // Opening file in reading mode
       strcpy(filename, (promo+i)->dire);
-      strcat(filename, entry->d_name);
+      strcat(filename, file->d_name);
+      printf("%s\n", filename);
       ptr_file = fopen(filename, "r");
       if (NULL == ptr_file) {
           printf("file can't be opened \n");
       }
+      printf("file opened \n");
       // reading first line of file
-      fgets(note ,3,ptr_file);
+      fgets(note, 3, ptr_file);
       // Closing the file
       fclose(ptr_file);
 
@@ -104,32 +132,34 @@ int envoie_numeros_operateur(int socketfd, char *data){
       (promo+i)->notes.notes[j] = atoi(note);
       (promo+i)->notes.nbr_notes++;
       printf("%i\n", (promo+i)->notes.notes[j]);
+      printf("%i\n", (promo+i)->notes.nbr_notes);
     }
+    closedir(dirp);
   }
   
   return(EXIT_SUCCESS);
 };
 
-
 int envoie_recois_message(int socketfd){
   char data[1024];
-  // la réinitialisation de l'ensemble des données
+  // la réinitialisation de l'ensemble de la donnée
   memset(data, 0, sizeof(data));
 
   // Demandez à l'utilisateur d'entrer un message
   char message[1024];
   printf("Votre message (max 1023 caracteres): \nCommencez par 'message:' ou 'calcule:' (ex : 'caclule: 2 3 +') ==> ");
   fgets(message, sizeof(message), stdin);
-  //strcpy(data, "message: ");
   strcat(data, message);
 
-  char code[11];
+  char code[10];
   memset(code, 0, sizeof(code));
   sscanf(data, "%s", code);
   printf("'%s'\n", code);
+  printf("%s\n", data);
+
   if (strcmp(code, "message:") == 0) envoie_message(socketfd, data);
-  if (strcmp(code, "calcule:") == 0) envoie_message(socketfd,data);
-  if (strcmp(code, "calculeM:") == 0) envoie_numeros_operateur(socketfd,data); 
+  if (strcmp(code, "calcule:") == 0) envoie_message(socketfd, data);
+  if (strcmp(code, "calculeM:") == 0) envoie_numeros_operateur(socketfd, data); 
 
   // la réinitialisation de l'ensemble des données
   memset(data, 0, sizeof(data));
